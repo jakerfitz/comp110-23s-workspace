ace50dd980bcace5555cb4d358c884095bd6bfac
Jacob Fitzgerald <jfitzgerald@unc.edu>
2023-01-30 19:05:23 -0500

    trial

---
+++ exercises/exercises/exercises/exercises/ex01_chardle.py
@@ -0,0 +1,51 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730474841"
+
+word = input("Enter a 5-character word: ")
+
+if (len(word) < 5):
+    print("Error: Word must contain 5 characters")
+    exit()
+if (len(word) > 5):
+    print("Error: Word must contain 5 characters")
+    exit()
+
+
+letter = input("Enter a single character: ")
+
+if len(letter) < 1:
+    print("Error: Character must be a single character.")
+    exit()
+if len(letter) > 1:
+    print("Error: Character must be a single character.")
+    exit()
+
+print("Searching for " + letter + " in " + word)
+if (word[0] == letter):
+    print(letter + " found at index 0")
+if (word[1] == letter):
+    print(letter + " found at index 1")
+if (word[2] == letter):
+    print(letter + " found at index 2")
+if (word[3] == letter):
+    print(letter + " found at index 3")
+if (word[4] == letter):
+    print(letter + " found at index 4")
+
+value = 0
+if (word[0] == letter):
+    value = value + 1
+if (word[1] == letter):
+    value = value + 1
+if (word[2] == letter):
+    value = value + 1
+if (word[3] == letter):
+    value = value + 1
+if (word[4] == letter):
+    value = value + 1
+
+if (value >= 1):
+    print(str(value) + " instances of " + str(letter) + " found in " + str(word))
+else:
+    print("No instances of " + str(letter) + "found in " + str(word))
---
+++ exercises/lessons/random_work.py
@@ -0,0 +1,6 @@
+"""just me messing around"""
+i = input("Pick a number between 1 and 100: ")
+for i in range(0,5):
+    print(str(i) + " is pretty small, probably the size of ur ...")
+for i in range(6,100):
+    print(str(i) + " is decent, but my favorite number is " + str(i == i + 1) + " and that's a bit bigger")
